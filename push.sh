c553822bc090dc4b99cc791cf71d52d0fbb5d33a
RTXUX <rtxux@ustclug.org>
2023-03-18 07:07:34 +0000

    push unconditionally

--- push.sh
+++ push.sh
@@ -2,13 +2,6 @@
 
 [[ $DEBUG = true ]] && set -x
 
-if [[ $GITHUB_EVENT_NAME != push && $GITHUB_EVENT_NAME != schedule ]]; then
-    exit 0
-fi
-if [[ $GITHUB_EVENT_NAME == push && $GITHUB_REF != "refs/heads/master" ]]; then
-    exit 0
-fi
-
 [[ -z $SKIP_LOGIN ]] && docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
 for image in $(docker images --filter=label=org.ustcmirror.images=true --format="{{.Repository}}:{{.Tag}}")
