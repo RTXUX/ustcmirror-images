2f179a6653eb20ff953eacb06360035edd78d5c8
RTXUX <rtxux@ustclug.org>
2023-03-18 08:32:01 +0000

    use ustcmirror-test

--- push.sh
+++ push.sh
@@ -2,8 +2,6 @@
 
 [[ $DEBUG = true ]] && set -x
 
-[[ -z $SKIP_LOGIN ]] && docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-
 for image in $(docker images --filter=label=org.ustcmirror.images=true --format="{{.Repository}}:{{.Tag}}")
 do
     docker push "$image"
